8cfc0db8aa81374f3d5a410e6cca19b7366eb3cc
hctramprn <hctramprn@users.noreply.github.com>
2022-03-25 20:44:26 -0600

    Create a file with handling error examples

---
+++ debugging.py
@@ -0,0 +1,28 @@
+#List comprehension containing all the divisors for the given number
+def divisors(num):
+    divisors = [number for number in range(1, num + 1) if num % number == 0]
+    return divisors
+
+#Validating the user's input using try and except
+def run():
+    try:
+        num = int(input('Choose a number: '))
+        if num < 0:
+            raise ValueError
+        print(divisors(num))
+    except ValueError:
+        print('The number must be greater than 0')
+    finally:
+        print('Ending program')
+
+
+#Validating the user's input using assert statements
+def run():
+    num = input('Choose a number: ')
+    assert num.isnumeric(), 'The number must be greater than 0'
+    assert num.isalnum(), 'The value can not contain letters'
+    print(divisors(int(num)))
+
+
+if __name__ == '__main__':
+    run()
